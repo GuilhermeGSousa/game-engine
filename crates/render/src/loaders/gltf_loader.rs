5b4aa6594c075e5d1086002011f57b7e870b4b44
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2025-10-09 23:18:38 +0200

    GLTF loader progress

--- crates/render/src/loaders/gltf_loader.rs
+++ crates/render/src/loaders/gltf_loader.rs
@@ -3,18 +3,28 @@ use ecs::{
     command::CommandQueue, component::Component, entity::Entity, query::Query, resource::Res,
 };
 use essential::assets::{
-    asset_loader::AssetLoader, asset_store::AssetStore, handle::AssetHandle, Asset, LoadableAsset,
+    asset_loader::AssetLoader, asset_server::AssetServer, asset_store::AssetStore,
+    handle::AssetHandle, Asset, LoadableAsset,
 };
+use gltf::{buffer::Data, Primitive};
+use wgpu::hal::auxil::db::amd::VENDOR;
 
 use crate::assets::{
-    texture::{Texture, TextureUsageSettings},
-    vertex::Vertex,
+    material::Material, mesh::Mesh, texture::{Texture, TextureUsageSettings}, vertex::Vertex
 };
 pub(crate) struct GLTFLoader;
 
-pub struct GLTFScene {}
+pub struct GLTFScene {
+    pub(crate) meshes: Vec<GLTFMesh>,
+    pub(crate) materials: Vec<AssetHandle<Material>>
+}
 
 impl Asset for GLTFScene {}
+
+pub struct GLTFMesh {
+    pub(crate) primitives: Vec<AssetHandle<Mesh>>,
+    pub(crate) materials: Vec<Option<usize>>
+}
 
 impl LoadableAsset for GLTFScene {
     type UsageSettings = ();
@@ -42,136 +52,124 @@ impl AssetLoader for GLTFLoader {
         let (document, buffers, images) = gltf::import(path.to_path()).unwrap();
 
         let mut textures = images.iter().enumerate().map(|(index, image)| {
-            if let Ok(asset) = Texture::from_bytes(&image.pixels, TextureUsageSettings::default()) {
-                Some(load_context.asset_server().add(asset))
-            } else {
-                None
-            }
+            let texture = Texture::from_bytes(&image.pixels, TextureUsageSettings::default())?;
+            load_context.asset_server().add(texture)
         });
 
-        // if textures.any(|handle| handle.is_none()) {
-        //     return Err(());
-        // }
+        let mut textures = VENDOR
 
-        // let textures: Vec<_> = textures.map(|handle| handle.unwrap()).collect();
+        let mut meshes = Vec::new();
+        for mesh in document.meshes() {
 
-        // let meshes = document
-        //     .meshes()
-        //     .map(|mesh: gltf::Mesh<'_>| {
-        //         let mut primitives = mesh.primitives().map(|gltf_primitive| {
-        //             let mut primitive = Primitive {
-        //                 vertices: Vec::new(),
-        //                 indices: Vec::new(),
-        //                 material_index: 0,
-        //             };
+            let mut primitives = Vec::new();
+            let mut materials = Vec::new();
+            for gltf_primitive in mesh.primitives() {
+                primitives.push(GLTFLoader::load_primitive(&buffers, &gltf_primitive, load_context.asset_server())?);
+                materials.push(gltf_primitive.material().index());
+            }
 
-        //             gltf_primitive
-        //                 .material()
-        //                 .occlusion_texture()
-        //                 .unwrap()
-        //                 .texture()
-        //                 .index();
+            meshes.push(GLTFMesh {
+                primitives,
+                materials
+            });
+        }
 
-        //             let reader = gltf_primitive.reader(|buffer| Some(&buffers[buffer.index()]));
+        Ok(GLTFScene { meshes, materials: vec![] })
+    }
+}
 
-        //             primitive.indices = match reader.read_indices()? {
-        //                 gltf::mesh::util::ReadIndices::U8(iter) => iter.map(|i| i as u32).collect(),
-        //                 gltf::mesh::util::ReadIndices::U16(iter) => {
-        //                     iter.map(|i| i as u32).collect()
-        //                 }
-        //                 gltf::mesh::util::ReadIndices::U32(iter) => iter.collect(),
-        //             };
+impl GLTFLoader {
+    fn load_primitive(
+        buffers: &Vec<Data>,
+        gltf_primitive: &Primitive,
+        asset_server: &AssetServer,
+    ) -> Result<AssetHandle<Mesh>, ()> {
+        let mut primitive = Mesh {
+            vertices: Vec::new(),
+            indices: Vec::new(),
+        };
 
-        //             primitive.vertices = reader
-        //                 .read_positions()?
-        //                 .map(|pos| Vertex {
-        //                     pos_coords: pos,
-        //                     uv_coords: [0.0; 2],
-        //                     normal: [0.0, 0.0, 1.0],
-        //                     tangent: [0.0; 3],
-        //                     bitangent: [0.0; 3],
-        //                     bone_indices: [0; Vertex::MAX_AFFECTED_BONES],
-        //                     bone_weights: [0.0; Vertex::MAX_AFFECTED_BONES],
-        //                 })
-        //                 .collect();
+        let reader = gltf_primitive.reader(|buffer| Some(&buffers[buffer.index()]));
 
-        //             if let Some(uv_0) = reader.read_tex_coords(0) {
-        //                 match uv_0 {
-        //                     gltf::mesh::util::ReadTexCoords::F32(iter) => {
-        //                         iter.enumerate().for_each(|(index, uvs)| {
-        //                             primitive.vertices[index].uv_coords = uvs;
-        //                         });
-        //                     }
-        //                     _ => return None,
-        //                 }
-        //             }
+        primitive.indices = match reader.read_indices().ok_or(())? {
+            gltf::mesh::util::ReadIndices::U8(iter) => iter.map(|i| i as u32).collect(),
+            gltf::mesh::util::ReadIndices::U16(iter) => iter.map(|i| i as u32).collect(),
+            gltf::mesh::util::ReadIndices::U32(iter) => iter.collect(),
+        };
+        primitive.vertices = reader
+            .read_positions()
+            .ok_or(())?
+            .map(|pos| Vertex {
+                pos_coords: pos,
+                uv_coords: [0.0; 2],
+                normal: [0.0, 0.0, 1.0],
+                tangent: [0.0; 3],
+                bitangent: [0.0; 3],
+                bone_indices: [0; Vertex::MAX_AFFECTED_BONES],
+                bone_weights: [0.0; Vertex::MAX_AFFECTED_BONES],
+            })
+            .collect();
 
-        //             if let Some(normals) = reader.read_normals() {
-        //                 normals.enumerate().for_each(|(index, normal)| {
-        //                     primitive.vertices[index].normal = normal;
-        //                 });
-        //             }
+        if let Some(uv_0) = reader.read_tex_coords(0) {
+            match uv_0 {
+                gltf::mesh::util::ReadTexCoords::F32(iter) => {
+                    iter.enumerate().for_each(|(index, uvs)| {
+                        primitive.vertices[index].uv_coords = uvs;
+                    });
+                }
+                _ => return Err(()),
+            }
+        }
 
-        //             if let Some(tangents) = reader.read_tangents() {
-        //                 tangents.enumerate().for_each(|(index, tangent)| {
-        //                     primitive.vertices[index].tangent =
-        //                         [tangent[0], tangent[1], tangent[2]];
-        //                 });
-        //             }
+        if let Some(normals) = reader.read_normals() {
+            normals.enumerate().for_each(|(index, normal)| {
+                primitive.vertices[index].normal = normal;
+            });
+        }
 
-        //             if let Some(joints_0) = reader.read_joints(0) {
-        //                 match joints_0 {
-        //                     gltf::mesh::util::ReadJoints::U8(iter) => {
-        //                         iter.enumerate().for_each(|(index, joint)| {
-        //                             primitive.vertices[index].bone_indices = [
-        //                                 joint[0].into(),
-        //                                 joint[1].into(),
-        //                                 joint[2].into(),
-        //                                 joint[3].into(),
-        //                             ];
-        //                         })
-        //                     }
-        //                     gltf::mesh::util::ReadJoints::U16(iter) => {
-        //                         iter.enumerate().for_each(|(index, joint)| {
-        //                             primitive.vertices[index].bone_indices = [
-        //                                 joint[0].into(),
-        //                                 joint[1].into(),
-        //                                 joint[2].into(),
-        //                                 joint[3].into(),
-        //                             ];
-        //                         })
-        //                     }
-        //                 }
-        //             }
+        if let Some(tangents) = reader.read_tangents() {
+            tangents.enumerate().for_each(|(index, tangent)| {
+                primitive.vertices[index].tangent = [tangent[0], tangent[1], tangent[2]];
+            });
+        }
 
-        //             if let Some(weights_0) = reader.read_weights(0) {
-        //                 match weights_0 {
-        //                     gltf::mesh::util::ReadWeights::F32(iter) => {
-        //                         iter.enumerate().for_each(|(index, weight)| {
-        //                             primitive.vertices[index].bone_weights = weight;
-        //                         });
-        //                     }
-        //                     _ => return None,
-        //                 }
-        //             }
+        if let Some(joints_0) = reader.read_joints(0) {
+            match joints_0 {
+                gltf::mesh::util::ReadJoints::U8(iter) => {
+                    iter.enumerate().for_each(|(index, joint)| {
+                        primitive.vertices[index].bone_indices = [
+                            joint[0].into(),
+                            joint[1].into(),
+                            joint[2].into(),
+                            joint[3].into(),
+                        ];
+                    })
+                }
+                gltf::mesh::util::ReadJoints::U16(iter) => {
+                    iter.enumerate().for_each(|(index, joint)| {
+                        primitive.vertices[index].bone_indices = [
+                            joint[0].into(),
+                            joint[1].into(),
+                            joint[2].into(),
+                            joint[3].into(),
+                        ];
+                    })
+                }
+            }
+        }
 
-        //             Some(primitive)
-        //         });
+        if let Some(weights_0) = reader.read_weights(0) {
+            match weights_0 {
+                gltf::mesh::util::ReadWeights::F32(iter) => {
+                    iter.enumerate().for_each(|(index, weight)| {
+                        primitive.vertices[index].bone_weights = weight;
+                    });
+                }
+                _ => return Err(()),
+            }
+        }
 
-        //         if primitives.any(|primitive| primitive.is_none()) {
-        //             Err(())
-        //         } else {
-        //             Ok(Mesh {
-        //                 primitives: primitives.map(|primitive| primitive.unwrap()).collect(),
-        //                 materials: vec![],
-        //             })
-        //         }
-        //     })
-        //     .collect::<Vec<_>>();
-
-        // Ok(Scene {})
-
-        Err(())
+        Ok(asset_server.add(primitive))
     }
 }
 

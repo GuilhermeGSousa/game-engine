b35edba0e4ea4f1d1de4934acf84d6d9d04a9365
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2025-12-07 16:29:03 +0100

    Implment missing access functions

--- crates/ecs/src/system/access.rs
+++ crates/ecs/src/system/access.rs
@@ -37,7 +37,64 @@ impl SystemAccess {
         self.writes_all = true;
     }
 
-    pub fn is_compatible(_other: &Self) -> bool {
-        todo!()
+    pub fn is_compatible(&self, other: &Self) -> bool {
+        if self.writes_all || other.writes_all {
+            return false;
+        }
+
+        // Self reads all and other writes any
+        if self.reads_all
+            && (!other.component_writes.is_empty() || !other.resource_writes.is_empty())
+        {
+            return false;
+        }
+
+        // Other reads all and self writes any
+        if other.reads_all
+            && (!self.component_writes.is_empty() || !self.resource_writes.is_empty())
+        {
+            return false;
+        }
+
+        // Self reads component and other writes
+        if !self.component_reads.is_disjoint(&other.component_writes) {
+            return false;
+        }
+
+        // Self writes component and other reads
+        if !self.component_writes.is_disjoint(&other.component_reads) {
+            return false;
+        }
+
+        // Both write component
+        if !self.component_writes.is_disjoint(&other.component_writes) {
+            return false;
+        }
+
+        // Self reads resource and other writes
+        if !self.resource_reads.is_disjoint(&other.resource_writes) {
+            return false;
+        }
+
+        // Self writes resource and other reads
+        if !self.resource_writes.is_disjoint(&other.resource_reads) {
+            return false;
+        }
+
+        // Both write resource
+        if !self.resource_writes.is_disjoint(&other.resource_writes) {
+            return false;
+        }
+
+        return true;
+    }
+
+    pub fn combine(&mut self, other: Self) {
+        self.component_reads.extend(other.component_reads);
+        self.component_writes.extend(other.component_writes);
+        self.resource_reads.extend(other.resource_reads);
+        self.resource_writes.extend(other.resource_writes);
+        self.reads_all |= other.reads_all;
+        self.writes_all |= other.writes_all;
     }
 }

a141785e389d68884e634e2771925e734fb19fba
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2026-03-30 09:17:22 +0200

    More progress

--- crates/ecs/src/system/batch.rs
+++ crates/ecs/src/system/batch.rs
@@ -1,11 +1,1 @@
-use crate::{system::ScheduledSystem, world::UnsafeWorldCell, System};
-
-pub(crate) struct SystemBatch {
-    systems: Vec<ScheduledSystem>,
-}
-
-impl SystemBatch {
-    pub(crate) fn run<'world>(&mut self, world: UnsafeWorldCell<'world>) {
-        self.systems.iter_mut().for_each(|system| system.run(world));
-    }
-}
+pub(crate) struct ScheduledBatch {}

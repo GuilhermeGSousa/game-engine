c9b728e7526db697952eb0aa40de8378c6633a75
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2026-04-15 11:50:06 +0200

    Schedule compilation progress

--- crates/ecs/src/system/schedule.rs
+++ crates/ecs/src/system/schedule.rs
@@ -10,7 +10,7 @@ use crate::{
     Resource,
 };
 use derive_more::{Deref, From};
-use petgraph::graph::NodeIndex;
+use petgraph::{algo::toposort, graph::NodeIndex};
 
 #[derive(Clone, Copy, Eq, Hash, PartialEq, Deref, From)]
 pub(crate) struct SystemNodeIndex(NodeIndex);
@@ -74,55 +74,15 @@ impl Schedule {
         self
     }
 
-    fn compile_batches(self) -> Vec<ScheduledBatch> {
-        // Compute in-degree of each node
-        // let mut in_degrees = vec![0usize; self.systems.len()];
-        // for system_id in &self.system_ids {
-        //     for system_edge in self.graph.edges(**system_id) {
-        //         let system_edge_target = self.graph.node_weight(system_edge.target()).unwrap();
-        //         in_degrees[*system_edge_target] += 1;
-        //     }
-        // }
+    pub fn compile(mut self) -> CompiledSchedule {
+        for node_id in toposort(&self.graph, None).expect("Error compiling schedule, cycle found") {
+        }
 
-        // let mut ready_queue = Vec::new();
-        // for system_index in 0..self.system_ids.len() {
-        //     if in_degrees[system_index] == 0 {
-        //         ready_queue.push(system_index);
-        //     }
-        // }
-
-        let mut processed_nodes = 0;
-        let mut batches = Vec::new();
-        // while processed_nodes < self.system_ids.len() {
-        //     let candidates = ready_queue.drain(..).collect::<Vec<_>>();
-        //     let mut batch = ScheduledBatch::default();
-
-        //     // Put all the disjoint systems in the same batch. Those that aren't will go on the next one
-        //     for candidate_index in candidates {
-        //         let candidate_system_access = &self.cached_access[candidate_index];
-        //         if batch.is_disjoint_from(candidate_system_access) {
-        //             batch.push(candidate_index, candidate_system_access.clone());
-
-        //             // Unlock successors of this node
-        //             for edge in self.graph.edges(*self.system_ids[candidate_index]) {
-        //                 let successor = self.graph.node_weight(edge.target()).unwrap();
-        //                 in_degrees[*successor] -= 1;
-        //                 // In Degree zero: this node is ready to be added to the next batch
-        //                 if in_degrees[*successor] == 0 {
-        //                     ready_queue.push(*successor);
-        //                 }
-        //             }
-        //             processed_nodes += 1;
-        //         } else {
-        //             unreachable!("This should never happen wtf");
-        //         }
-        //     }
-        //     batches.push(batch);
-        // }
-        batches
-    }
-
-    pub fn compile(self) -> CompiledSchedule {
+        let sorted_nodes = toposort(&self.graph, None)
+            .expect("Error compiling schedule, cycle found")
+            .into_iter()
+            .map(|node_id| self.graph.remove_node(node_id).unwrap())
+            .collect::<Vec<_>>();
         todo!()
     }
 }
@@ -249,7 +209,7 @@ mod tests {
     #[test]
     fn schedule_new() {
         let schedule = Schedule::new();
-        assert_eq!(schedule.graph.node_count(), 2);
+        assert_eq!(schedule.graph.node_count(), 0);
         assert_eq!(schedule.system_ids.len(), 0);
     }
 
@@ -281,39 +241,5 @@ mod tests {
         assert_eq!(schedule.graph.node_count(), 3);
         assert_eq!(schedule.system_ids.len(), 3);
         assert_eq!(schedule.graph.node_count(), 3);
-    }
-
-    #[test]
-    fn system_batching_empty() {
-        let schedule = Schedule::new();
-
-        let batches = schedule.compile_batches();
-        assert!(batches.is_empty())
-    }
-
-    #[derive(Component)]
-    struct ComponentA;
-
-    #[derive(Component)]
-    struct ComponentB;
-
-    #[test]
-    fn system_batching_two_disjoint() {
-        let mut schedule = Schedule::new();
-        schedule
-            .add_system(|_foo: Query<&ComponentA>| {})
-            .add_system(|_foo: Query<&ComponentB>| {});
-        let batches = schedule.compile_batches();
-        assert!(batches.len() == 1)
-    }
-
-    #[test]
-    fn system_batching_two_not_disjoint() {
-        let mut schedule = Schedule::new();
-        schedule
-            .add_system(|_foo: Query<(&ComponentA, &ComponentB)>| {})
-            .add_system(|_foo: Query<&mut ComponentA>| {});
-        let batches = schedule.compile_batches();
-        assert!(batches.len() == 2)
     }
 }

c5ae21a7e6c1243f5f0eb762ec8f50f4bef08ae2
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2026-08-07 23:37:16 +0200

    Extract system input

--- crates/app/src/extractor.rs
+++ crates/app/src/extractor.rs
@@ -1,7 +1,10 @@
 use std::mem;
 
-use derive_more::Deref;
-use ecs::{Resource, World};
+use derive_more::{Deref, DerefMut};
+use ecs::{
+    system::input::{SystemInput, SystemInputData},
+    Resource, World,
+};
 
 #[derive(Deref, Default, Resource)]
 pub(crate) struct ScratchMainWorld(pub(crate) World);
@@ -29,4 +32,33 @@ pub(crate) fn extract(main: &mut World, other: &mut World) {
     let moved_main = other.remove_resource::<MainWorld>().unwrap();
     let scratch_world = mem::replace(main, moved_main.0);
     main.insert_resource(ScratchMainWorld(scratch_world));
+}
+
+#[derive(Deref, DerefMut)]
+pub struct Extract<'world, 'state, T: SystemInput>(SystemInputData<'world, 'state, T>);
+
+impl<T: SystemInput> SystemInput for Extract<'_, '_, T> {
+    type State = T::State;
+
+    type Data<'world, 'state> = Extract<'world, 'state, T>;
+
+    fn init_state() -> Self::State {
+        T::init_state()
+    }
+
+    fn get_data<'world, 'state>(
+        state: &'state mut Self::State,
+        world: ecs::world::UnsafeWorldCell<'world>,
+    ) -> Self::Data<'world, 'state> {
+        let main_world = world
+            .world()
+            .get_resource::<MainWorld>()
+            .expect("MainWorld not found — `Extract` is only valid in the `Extract` update group");
+
+        Extract(T::get_data(state, main_world.as_unsafe_world_cell()))
+    }
+
+    fn fill_access(access: &mut ecs::system::access::SystemAccess) {
+        access.read_resource::<MainWorld>();
+    }
 }
